d72f32e06a9ab7a559db4dd6035e503c7c37b6d0
Dio Rizqi <124369676+diorizqi404@users.noreply.github.com>
2023-11-25 18:22:26 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,3 +1,7 @@
 function calculateNumbers(value1, value2) {
-    return value1 + value2;
+  const sum = value1 + value2;
+  const diff = value1 - value2;
+  const mult = value1 * value2;
+  const div = value1 / value2;
+  return [sum, diff, mult, div];
 }
